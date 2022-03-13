cee7e8f035db82856b65ab23bbf373e9f4435beb
Almutasm Hesheiam <95537675+AlmutasmHesheiam@users.noreply.github.com>
2022-03-13 17:59:39 +0300

    Add files via upload

---
+++ UsingGUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class UsingGUI {
+    private JLabel Fname;
+    private JTextField tfName;
+    private JTextField cridetCard;
+    private JButton btnExit;
+    private JButton btnEnter;
+    private JPanel mainPanel;
+    private JLabel crCard;
+    private JLabel lbWelcome;
+}
